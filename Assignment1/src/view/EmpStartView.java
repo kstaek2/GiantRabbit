45350759729ab0786770a437895d4edd77ea20e3
0131ksy <@SHINY>
2014-03-27 21:20:51 +0900

    준이진짜천재

--- Assignment1/src/view/EmpStartView.java
+++ Assignment1/src/view/EmpStartView.java
@@ -10,6 +10,8 @@ public class EmpStartView {
 	public static void main(String[] args) {
 		System.out.println("ㅎㅇhihello");
 		System.out.println("준이천재바보");
+		
+		System.out.println("준이진짜천재");
 		//emp01 데이터 모두 출력
 		System.out.println("*** emp01 데이터 모두 출력 ***");
 		EmpController.getAllEmp();
