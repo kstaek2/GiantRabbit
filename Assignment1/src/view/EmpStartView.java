a4aa046e5e4255942087d637cc59f37ffde740c9
0131ksy <@SHINY>
2014-03-27 21:09:26 +0900

    준이천재짱님

--- Assignment1/src/view/EmpStartView.java
+++ Assignment1/src/view/EmpStartView.java
@@ -9,6 +9,7 @@ import model.domain.Emp;
 public class EmpStartView {
 	public static void main(String[] args) {
 		System.out.println("ㅎㅇhihello");
+		System.out.println("준이천재");
 		//emp01 데이터 모두 출력
 		System.out.println("*** emp01 데이터 모두 출력 ***");
 		EmpController.getAllEmp();
